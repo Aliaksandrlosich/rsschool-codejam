80b3413b165bca076a34ce59eb4d51a207c60071
Sanyu <sa.los@mail.ru>
2018-10-28 19:44:38 +0200

    refactor

--- sum.js
+++ sum.js
@@ -2,22 +2,25 @@ const assert = require("assert");
 
 function sumofother(array) 
 { let result = [];
-     let sumArray = array.reduce(function(a, b) {
+  let length=array.length;
+    let sumArray = array.reduce(function(a, b) {
   return a + b;
 });
-     let length=array.length;
     for(let i = 0; i < length; i++)
       result.push(sumArray-array[i]);
- return result
+ return result;
 };
 
 describe('Sum of other', () => {
   it('1', () => {
-    const array = sumofother([ 2, 3, 4, 1 ]);
-    assert.deepEqual(array, [ 8, 7, 6, 9 ]);
-  });
+    const array = sumofother([2, 3 ,4 ,1]);
+    const exit = [8, 7, 6, 9];
+    assert.deepEqual(array, exit);
+                }
+    );
   it('2', () => {
-    const array = sumofother([ 5, 0, 6, -1 ]);
-    assert.deepEqual(array, [ 5, 10, 4, 11 ]);
-  });
+    const array = sumofother([5, 0, 6, -1]);
+    const exit = [5, 10, 4, 11];
+    assert.deepEqual(array, exit);
+} );
 });
